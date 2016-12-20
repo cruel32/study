10f76f1f3aacd94297d16aa1d721898bb585567c
cruel-32 <cruel32@nate.com>
2016-12-20 17:16:00 +0900

    study
    study

---
+++ es6/html/math.js
@@ -0,0 +1,8 @@
+"use strict"
+import * as logarithm from "math_modules/lograrithm";
+import * as trigonometry from "math_modules/trigonometry";
+
+export default {
+	logarithm : logarithm,
+	trigonometry : trigonometry
+}
---
+++ es6/html/math_modules/trigonometry.js
@@ -0,0 +1,13 @@
+"use strict"
+var cos = Math.cos;
+var sin = Math.sin;
+
+function getSin(value){
+	return sin(value);
+}
+
+function getCos(value){
+	return cos(value);
+}
+
+export {getCos, getSin};
