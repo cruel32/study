579fe83b5713f6dee1937e038ae1ee47eae58061
cruel32 <cshee8508@gmail.com>
2017-02-28 00:18:54 +0900

    scu

---
+++ scu/비쥬얼C++/lecture/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+inline int maximum(int a, int b);
+int main()
+{
+    int a = maximum(1, 2);
+    int b = maximum(3, 2);
+    std::cout << "a = " << a << ", b = " << b <<
+    return 0;
+}
