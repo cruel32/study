9da2c76d913dfa2877fab797e1192672a2c0b90d
cruel32 <cshee8508@gmail.com>
2017-03-04 01:04:22 +0900

    angular2

--- angular2/hello-ng2/src/app/helloComponent/hello.component.ts
+++ angular2/hello-ng2/src/app/helloComponent/hello.component.ts
@@ -3,11 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'hello',
   template: `
-    <h1 class="sec_txt">{{title}}</h1>
-    <input type="text" [(ngModel)]="title" />
-    <textarea [(ngModel)]="title"></textarea>
+    <div class="wrap">
+      <h1 class="sec_txt line blue">{{title}}</h1>
+      <input type="text" [(ngModel)]="title" />
+      <textarea [(ngModel)]="title"></textarea>
+    </div>
   `,
-  styles: [`.sec_txt {color:orange}`]
+  styleUrls: ['hello.scss']
 })
 export class HelloComponent {
   title = 'it works';
--- scu/비쥬얼C++/lecture/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-inline int maximum(int a, int b);
-int main()
-{
-    int a = maximum(1, 2);
-    int b = maximum(3, 2);
-    std::cout << "a = " << a << ", b = " << b <<
-    return 0;
-}
