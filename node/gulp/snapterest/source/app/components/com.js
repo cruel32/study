073064a4909a0d73c2301bdd3f1b06edcd809d31
cshee8508@gmail.com <cshee8508@gmail.com>
2017-05-22 01:31:33 +0900

    react

--- node/gulp/snapterest/source/app/components/com.js
+++ node/gulp/snapterest/source/app/components/com.js
@@ -1,11 +1,11 @@
 module.exports.test = {
     test1 : function(){
-        console.log("this : ", this);
+        console.log("this test1 : ", this);
     },
     test2 : ()=>{
-        console.log("this : ", this);
+        console.log("this test2 : ", this);
     },
     test3(){
-        console.log("this : ", this);
+        console.log("this  test3: ", this);
     }
 }
