1bdf43a202ab5d96fd6cb70d100a407ae0bb009e
cruel32 <cshee8508@gmail.com>
2017-05-27 03:15:44 +0900

    re-react

---
+++ react/source/app/app.js
@@ -0,0 +1,5 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+let Application = require('./components/Application.react');
+
+ReactDOM.render(<application/>, document.getElementById('react-application'));
