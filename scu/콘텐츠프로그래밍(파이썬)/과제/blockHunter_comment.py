a0eac9c84d67758d4583fb0fdc2cdd2e7ded78c2
cruel32 <cshee8508@gmail.com>
2017-06-09 22:15:04 +0900

    scu

--- scu/콘텐츠프로그래밍(파이썬)/과제/blockHunter_comment.py
+++ scu/콘텐츠프로그래밍(파이썬)/과제/blockHunter_comment.py
@@ -11,26 +11,26 @@ destY = mc.getHeight(destX, destZ) #destX와 destZ를 인자로 mc.getHeight 함
 print(destX, destY, destZ) #위의 변수들을 출력합니다.
 
 block = 57 #block 변수에 57숫자를 저장합니다.
-mc.setBlock(destX, destY, destZ, block) #mc.setBlock을 destX, destY, destZ, block 변수들을 인자로 넘기고 호출합니다.
-mc.postToChat("Block set") #mc.postToChat을 "Block set" 문자열을 인자로 넘기고 호출합니다.
+mc.setBlock(destX, destY, destZ, block) #mc.setBlock으로 destX, destY, destZ, block 변수들을 인자로 넘기고 블록을 셋팅합니다.
+mc.postToChat("Block set") #mc.postToChat함수로 "Block set"을 마인크래프트로 채팅합니다.
 
 while True: #다음 코드를 영원히 반복합니다.
-    pos = mc.player.getPos() #pos변수에 mc.player.getPos() 리턴값을 저장합니다.
+    pos = mc.player.getPos() #pos변수에 mc.player.getPos() 리턴값, 플레이어의 위치를 저장합니다.
     distance = math.sqrt((pos.x - destX) ** 2 + (pos.z - destZ) ** 2)
     #pos.x에서 destX만큼을 빼고 2을 재곱한뒤 pos.z에서 destZ를 빼고 2제곱한 만큼의 값을 더한뒤 그의 제곱근을 구해 distance변수에 담습니다.
 
     if distance == 0: #distance가 0이 되면 반복문에서 빠져나옵니다.
         break
 
-    if distance > 100: #distance가 100이상이 되면 mc.postToChat를 호출합니다. (인자값은 "Freezing")
+    if distance > 100: #distance가 100이상이 되면 mc.postToChat로 다음을 출력합니다. ("Freezing")
         mc.postToChat("Freezing")
-    elif distance > 50: #distance가 50이상이 되면 mc.postToChat를 호출합니다. (인자값은 "Cold")
+    elif distance > 50: #distance가 50이상이 되면 mc.postToChat를 다음을 출력합니다. ("Cold")
         mc.postToChat("Cold")
-    elif distance > 25: #distance가 25이상이 되면 mc.postToChat를 호출합니다. (인자값은 "Warm")
+    elif distance > 25: #distance가 25이상이 되면 mc.postToChat를 다음을 출력합니다. ("Warm")
         mc.postToChat("Warm")
-    elif distance > 12: #distance가 12이상이 되면 mc.postToChat를 호출합니다. (인자값은 "Boiling")
+    elif distance > 12: #distance가 12이상이 되면 mc.postToChat를 다음을 출력합니다. ("Boiling")
         mc.postToChat("Boiling")
-    elif distance > 6: #distance가 6이상이 되면 mc.postToChat를 호출합니다. (인자값은 "On fire!")
+    elif distance > 6: #distance가 6이상이 되면 mc.postToChat를 다음을 출력합니다. ("On fire!")
         mc.postToChat("On fire!")
-    elif distance == 0: #distance가 0 이 되면 mc.postToChat를 호출합니다. (인자값은 "Found it")
+    elif distance == 0: #distance가 0 이 되면 mc.postToChat를 다음을 출력합니다. ("Found it")
         mc.postToChat("Found it")
