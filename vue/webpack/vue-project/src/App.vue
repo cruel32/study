5acfe7e6d333208181b9f7fda0a52b9e68eadb68
cruel32 <cshee8508@gmail.com>
2017-06-28 00:00:13 +0900

    vue

--- vue/webpack/vue-project/src/App.vue
+++ vue/webpack/vue-project/src/App.vue
@@ -9,17 +9,17 @@
     },
     data(){
       return {
-        items:[
-          {text:'Bananas', checked:true},
-          {text:'apple', checked:false},
+        items : [
+          {'text':'Bananas', 'checked':true},
+          {'text':'apple', 'checked':false}
         ]
       }
     },
     methods:{
       addItem(text){
         this.items.push({
-          text:text,
-          checked:false
+          'text':text,
+          'checked':false
         })
       }
     }
@@ -28,11 +28,11 @@
 </script>
 
 <template>
-  <div id="app" class="container">
+  <div id='app' class='container'>
     <h2>{{title}}</h2>  
     <add-item-component></add-item-component>
     <items-component></items-component>
-    <div class="footer">
+    <div class='footer'>
       <hr>
       <change-title-component></change-title-component>
     </div>
